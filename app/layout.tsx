710263073bdd6b88047524fc6869b33a3b73eb9d
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 22:29:12 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,19 @@
-. 
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MHBuilder Lahore Building Contractor",
+  description: "Construction Company in Lahore",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
