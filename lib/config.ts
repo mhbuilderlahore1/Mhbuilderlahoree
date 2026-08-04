b080bf11236c86870c91219ac676d214bd055a50
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 22:55:17 +0500

    Create config.ts

---
+++ lib/config.ts
@@ -0,0 +1,18 @@
+export const APP = {
+  name: "MHBuilder Lahore",
+  title: "MHBuilder Lahore Building Contractor",
+  description:
+    "Professional Building Contractor and Construction Company in Lahore.",
+
+  admin: {
+    username: "mhbuilderlahore",
+  },
+
+  seo: {
+    title: "MHBuilder Lahore",
+    description:
+      "Construction Company in Lahore",
+    keywords:
+      "builder, construction, lahore, contractor"
+  }
+};
