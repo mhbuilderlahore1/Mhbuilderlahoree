d59288df235cfc44905466e95ef52d4082790725
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 22:46:40 +0500

    Create Sidebar.tsx

---
+++ components/admin/Sidebar.tsx
@@ -0,0 +1,43 @@
+export default function Sidebar() {
+  return (
+    <aside>
+
+      <h2>MHBuilder CMS</h2>
+
+      <ul>
+
+        <li>Dashboard</li>
+
+        <li>Homepage</li>
+
+        <li>Pages</li>
+
+        <li>About</li>
+
+        <li>Services</li>
+
+        <li>Projects</li>
+
+        <li>Products</li>
+
+        <li>Gallery</li>
+
+        <li>Blog</li>
+
+        <li>Categories</li>
+
+        <li>Videos</li>
+
+        <li>Menus</li>
+
+        <li>Footer</li>
+
+        <li>SEO</li>
+
+        <li>Settings</li>
+
+      </ul>
+
+    </aside>
+  );
+}
