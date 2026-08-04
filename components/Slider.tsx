d0a81cac116fe4201ac61a7be34c3ffe40b85b2d
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 22:34:47 +0500

    Create Slider.tsx

---
+++ components/Slider.tsx
@@ -0,0 +1,11 @@
+export default function Slider() {
+  return (
+    <section className="slider">
+
+      <div className="slide">
+        Slider Images will come from Admin Panel.
+      </div>
+
+    </section>
+  );
+}
