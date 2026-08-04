44a511c9ca6e0c54c4e414562893225cce636b72
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 22:51:57 +0500

    Create page.tsx

---
+++ app/login/page.tsx
@@ -0,0 +1,31 @@
+export default function LoginPage() {
+  return (
+    <main>
+
+      <h1>MHBuilder CMS Login</h1>
+
+      <form>
+
+        <input
+          type="text"
+          placeholder="Username"
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+        />
+
+        <br /><br />
+
+        <button>
+          Login
+        </button>
+
+      </form>
+
+    </main>
+  );
+}
