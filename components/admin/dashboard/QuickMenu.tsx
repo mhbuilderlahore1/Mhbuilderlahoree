2a95194f189a1c64d8408b238a0b61dacd25618a
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 23:12:19 +0500

    Add QuickMenu component with navigation links

---
+++ components/admin/dashboard/QuickMenu.tsx
@@ -0,0 +1,31 @@
+export default function QuickMenu() {
+  return (
+    <div>
+
+      <h2>Quick Menu</h2>
+
+      <ul>
+
+        <li>Homepage</li>
+
+        <li>Pages</li>
+
+        <li>About</li>
+
+        <li>Services</li>
+
+        <li>Projects</li>
+
+        <li>Products</li>
+
+        <li>Gallery</li>
+
+        <li>Blog</li>
+
+        <li>Settings</li>
+
+      </ul>
+
+    </div>
+  );
+}
