57727aac2c3a305612cccbfd9a81f0b5af74030a
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 22:55:51 +0500

    Define TypeScript interfaces for Button, Page, Post, and Category

---
+++ types/index.ts
@@ -0,0 +1,22 @@
+export interface Button {
+  id: number;
+  title: string;
+  link: string;
+}
+
+export interface Page {
+  id: number;
+  title: string;
+  slug: string;
+}
+
+export interface Post {
+  id: number;
+  title: string;
+  slug: string;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+}
