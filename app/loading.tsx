f5b29a30ea3670814ad04e64f4c4177d238a3910
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 23:40:47 +0500

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+export default function Loading() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      <h2>Loading MHBuilder CMS...</h2>
+    </main>
+  );
+}
