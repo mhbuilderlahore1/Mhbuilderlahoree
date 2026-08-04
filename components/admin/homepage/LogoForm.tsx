55104c8e5ef6170389d5c500b094ab60e712b256
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 23:20:01 +0500

    Create LogoForm.tsx

---
+++ components/admin/homepage/LogoForm.tsx
@@ -0,0 +1,20 @@
+export default function LogoForm() {
+  return (
+    <div>
+
+      <h2>Company Logos</h2>
+
+      <input
+        type="file"
+        multiple
+      />
+
+      <br /><br />
+
+      <button>
+        Upload Logos
+      </button>
+
+    </div>
+  );
+}
