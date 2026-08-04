5ea86a4024d97d7b956678ced47b1d6203ff6461
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 22:40:48 +0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,16 @@
 export default function Header() {
   return (
-    <header className="header">
-      <div className="container">
-        <h2 className="logo">
-          MHBuilder Lahore
-        </h2>
+    <header>
+      <h2>MHBuilder Lahore</h2>
 
-        <nav>
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/services">Services</a>
-          <a href="/projects">Projects</a>
-          <a href="/blog">Blog</a>
-          <a href="/contact">Contact</a>
-        </nav>
-      </div>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/services">Services</a>
+        <a href="/projects">Projects</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+      </nav>
     </header>
   );
 }
