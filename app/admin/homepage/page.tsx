6da980fd9267442a55cfb421111fe22c29b1d187
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 23:21:40 +0500

    Update page.tsx

--- app/admin/homepage/page.tsx
+++ app/admin/homepage/page.tsx
@@ -2,6 +2,10 @@ import HeroForm from "@/components/admin/homepage/HeroForm";
 import SliderForm from "@/components/admin/homepage/SliderForm";
 import AboutForm from "@/components/admin/homepage/AboutForm";
 import ServicesForm from "@/components/admin/homepage/ServicesForm";
+import ProjectsForm from "@/components/admin/homepage/ProjectsForm";
+import BlogForm from "@/components/admin/homepage/BlogForm";
+import GalleryForm from "@/components/admin/homepage/GalleryForm";
+import LogoForm from "@/components/admin/homepage/LogoForm";
 
 export default function HomepageBuilder() {
   return (
@@ -10,12 +14,13 @@ export default function HomepageBuilder() {
       <h1>Homepage Builder</h1>
 
       <HeroForm />
-
       <SliderForm />
-
       <AboutForm />
-
       <ServicesForm />
+      <ProjectsForm />
+      <BlogForm />
+      <GalleryForm />
+      <LogoForm />
 
     </main>
   );
