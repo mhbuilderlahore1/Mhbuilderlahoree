8e081a61ffb576cef284a31e5eca936fb8cb4d18
Mhbuilder Lahore <mhbuilderlahore@gmail.com>
2026-08-04 23:14:56 +0500

    Create HomepageBuilder component with forms

---
+++ app/admin/homepage/page.tsx
@@ -0,0 +1,16 @@
+import HeroForm from "@/components/admin/homepage/HeroForm";
+import SliderForm from "@/components/admin/homepage/SliderForm";
+
+export default function HomepageBuilder() {
+  return (
+    <main>
+
+      <h1>Homepage Builder</h1>
+
+      <HeroForm />
+
+      <SliderForm />
+
+    </main>
+  );
+}
